e1e9116ccbad22749ebf8b7f7e0eea90a7a45be0
alexmickelson96 <alexmickelson96@gmail.com>
2019-03-04 16:25:59 -0700

    branched master

---
+++ SemesterCalendar.Droid/Views/DetailView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Platforms.Android.Views;
+using SemesterCalendar.Core.ViewModels;
+
+namespace SemesterCalendar.Droid.Views
+{
+    [Activity(Label = "SemesterList", MainLauncher = true)]
+    public class DetailView : MvxActivity<ListViewModel>
+    {
+    }
+}
